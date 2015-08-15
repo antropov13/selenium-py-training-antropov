0f45fc5afb6338ce38874fb3b1f111beebb654e3
Antropov Igor <igor.antropov13@gmail.com>
2015-08-15 23:14:51 +0600

    edit add_film - #3

--- php4dvd/test_add_film_an_error.py
+++ php4dvd/test_add_film_an_error.py
@@ -16,21 +16,6 @@ def is_find_element(self,by,locator):
     except WebDriverException:
         self.fail("time out: "+locator)
 
-def to_compare_text(self,by,locator,text):
-    driver = self.driver
-    text_site = driver.find_element(by, locator).text
-    if text.find(text_site)>(-1):
-        return True
-    else:
-        self.fail(text_site+" != "+ text)
-
-def to_compare_text2(self,text1,text2):
-    driver = self.driver
-    if text1==text2:
-        return True
-    else:
-        self.fail(text1+" != "+ text2)
-
 class Untitled2(unittest.TestCase):
 
     def setUp(self):
@@ -60,48 +45,25 @@ class Untitled2(unittest.TestCase):
         taglines = 'A new age begins'
         cast = ("Роберт Дауни мл.", "Крис Хемсворт", "Крис Эванс")
 
-        driver.find_element_by_name('name').send_keys(name)
+        #Не заполняем одно обязательное поле
         driver.find_element_by_name('year').send_keys(year)
         driver.find_element_by_name('taglines').send_keys(taglines)
         driver.find_element_by_id('own_no').click()
-
-        #Хотел прикрепить картинку..
-        #cover = os.path.abspath("img/cover.jpg")
-        #driver.find_element_by_xpath("//input[@type='file']").send_keys(cover)
-
-        #driver.find_element_by_id('cover').send_keys("img/cover.jpg")
 
         for element in cast:
             driver.find_element_by_name('cast').send_keys(element)
             driver.find_element_by_name('cast').send_keys(Keys.ENTER)
 
         driver.find_element_by_id('submit').click()
-        is_find_element(self, By.CSS_SELECTOR,"div.maininfo_full")
-        #Проверка
 
-        to_compare_text(self,By.CSS_SELECTOR,"div.maininfo_full > h2",name+" ("+year+")")
-        to_compare_text(self,By.CSS_SELECTOR,"div.taglines > div",taglines)
-
-        cast_cite = driver.find_elements_by_css_selector("div.maininfo_full > div.cast > div.castlist > div.column > ul > li")
-        assert len(cast_cite) == len(cast)
-        i = 0
-        for element in cast_cite:
-            to_compare_text2(self,element.text,cast[i])
-            i=i+1
+        is_find_element(self, By.CSS_SELECTOR,"div.addmovie")
 
         driver.find_element_by_css_selector("div.center > nav > ul > li > a").click()
         is_find_element(self, By.ID,"results")
 
         movies_after = driver.find_elements_by_css_selector("#results > a > div.movie_box > div.title")
-        assert len(movies_to)+1 == len(movies_after)
 
-        for movie in movies_after:
-            try:
-                if name == movie.text:
-                    break
-            except: pass
-        else:
-            self.fail("error movie add")
+        assert len(movies_to) == len(movies_after)
 
         driver.find_element_by_css_selector("nav > ul > li:nth-child(4) > a").click()
         driver._switch_to.alert.accept()
